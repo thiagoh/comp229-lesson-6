fff7dd1d906da4c2029af7b31b7ee6be8b99358d
Thiago Andrade <thiagoh@gmail.com>
2016-10-12 21:51:50 -0400

    added logout

--- comp229-lesson-6/Logout.aspx.cs
+++ comp229-lesson-6/Logout.aspx.cs
@@ -5,10 +5,23 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace comp229_lesson_6 {
     public partial class Logout : System.Web.UI.Page {
+
         protected void Page_Load(object sender, EventArgs e) {
 
+            // store session info and authentication methods in authenticationmanager object
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+
+            authenticationManager.SignOut();
+
+            // redirect
+            Response.Redirect("~/Login.aspx");
         }
     }
 }
--- comp229-lesson-6/Register.aspx.cs
+++ comp229-lesson-6/Register.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace comp229_lesson_6 {
     public partial class Register : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
@@ -13,10 +18,43 @@ namespace comp229_lesson_6 {
 
         protected void CancelButton_Click(object sender, EventArgs e) {
 
+            Response.Redirect("~/Default.aspx");
         }
 
         protected void RegisterButton_Click(object sender, EventArgs e) {
 
+            // create a new userStore and userManager objects
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            // search and create a new user object
+            var user = new IdentityUser {
+                UserName = UserNameTextBox.Text,
+                PhoneNumber= PhoneNumberTextBox.Text,
+                Email = EmailTextBox.Text,
+            };
+
+            //create a new user in the db and store the results
+            IdentityResult result = userManager.Create(user, PasswordTextBox.Text);
+
+            // check if successfully registered?
+
+            if (result.Succeeded) {
+
+                // authenticate and login the user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                authenticationManager.SignIn(new AuthenticationProperties(), userIdentity);
+
+                // redirect to main menu
+                Response.Redirect("~/Contoso/MainMenu.aspx");
+
+            } else {
+
+                StatusLabel.Text = result.Errors.FirstOrDefault();
+                AlertFlash.Visible = true;
+            }
         }
     }
 }
