222e50456e61fec660217249713e312b56968b78
Thiago Andrade <thiagoh@gmail.com>
2016-10-12 21:13:49 -0400

    authorization

---
+++ comp229-lesson-6/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+// required for OWIN Startup
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(comp229_lesson_6.Startup))]
+
+namespace comp229_lesson_6 {
+
+    public class Startup {
+
+        public void Configuration(IAppBuilder app) {
+
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            var authOpts =  new CookieAuthenticationOptions();
+            authOpts.AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie;
+            authOpts.LoginPath = new PathString("/Login.aspx");
+
+            app.UseCookieAuthentication(authOpts);
+        }
+    }
+}
